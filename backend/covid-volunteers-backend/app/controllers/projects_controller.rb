0fdc12e02437edb5bd33512138be73a40bf2636d
stabassum <sumu1993@gmail.com>
2020-07-23 17:06:46 -0400

    projects controller updated

--- backend/covid-volunteers-backend/app/controllers/projects_controller.rb
+++ backend/covid-volunteers-backend/app/controllers/projects_controller.rb
@@ -1,2 +1,22 @@
 class ProjectsController < ApplicationController
+
+    def index
+
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    def project_params
+    end
+
 end
