904fae42b7daf9a7a09835c3081284ff9efd0c13
stabassum <sumu1993@gmail.com>
2020-07-23 16:54:48 -0400

    volunteers controller updated

--- backend/covid-volunteers-backend/app/controllers/volunteers_controller.rb
+++ backend/covid-volunteers-backend/app/controllers/volunteers_controller.rb
@@ -1,2 +1,31 @@
 class VolunteersController < ApplicationController
+    def index
+        @volunteers = Volunteer.all
+        render json: @volunteers
+    end
+
+    def show
+        @volunteer = Volunteer.find(params[:id])
+        render json: @volunteer
+    end
+
+    def create
+        @volunteer = Volunteer.create(volunteer_params)
+        render json: @volunteer
+    end
+
+    # def update
+
+    # end
+
+    def destroy
+        volunteer = Volunteer.find_by(id: params[:id])
+        volunteer.destroy
+        render json: volunteer
+    end
+
+    def volunteer_params
+        params.permit(:name, :age, :contact, :skills)
+    end
+
 end
