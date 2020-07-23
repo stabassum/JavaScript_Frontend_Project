59b54b4fd44c502e47a043e5bc53b57c9c775496
stabassum <sumu1993@gmail.com>
2020-07-23 17:10:11 -0400

    projects controller updated

--- backend/covid-volunteers-backend/app/controllers/projects_controller.rb
+++ backend/covid-volunteers-backend/app/controllers/projects_controller.rb
@@ -1,22 +1,31 @@
 class ProjectsController < ApplicationController
 
     def index
-
+        @projects = Project.all
+        render json: @projects
     end
 
     def show
+        @project = Project.find(params[:id])
+        render json: @project
     end
 
     def create
+        @project = Project.create(project_params)
+        render json: @project
     end
 
-    def update
-    end
+    # def update
+    # end
 
     def destroy
+        project = Project.find_by(id: params[:id])
+        project.destroy
+        render json: project
     end
 
     def project_params
+        params.permit(:title, :description, :date, :location, :time, :volunteer_id)
     end
 
 end
