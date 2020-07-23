3e95e67ec8157b043cf3d16df29357e4e0c5b8e1
stabassum <sumu1993@gmail.com>
2020-07-23 18:17:44 -0400

    seed file updated

--- backend/covid-volunteers-backend/db/seeds.rb
+++ backend/covid-volunteers-backend/db/seeds.rb
@@ -6,3 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Volunteer.create(name: 'Maryam Khan', age:'38', contact: 'm.khan@gmail.com', skills: 'First Aid, Registerd Nurse, Emergency Medicine')
+
+Project.create(title: 'Nursing Home Volunteer', description: 'Help clean up nuring home thoroughly', date: '07/24/2020', location: "New York, NY", time: "9:00AM - 12:00PM", volunteer_id: 1)
+Project.create(title: 'Pre-School Volunteer', description: 'Clean up mess of pre-schoolers', date: '07/28/2020', location: "Albany, NY", time: "1:00PM - 3:00PM", volunteer_id: 1)
+
+Volunteer.create(name: 'Asif Akter', age:'32', contact: 'a.akter@gmail.com', skills: 'EMT Liscenced, Home Health Aide, First Aid, ER Volunteer Experience')
+
+Project.create(title: 'After-School Volunteer', description: 'Help instructors run activities', date: '08/01/2020', location: "Buffalo, NY", time: "3:00PM - 6:00PM", volunteer_id: 2)
+Project.create(title: 'ER Volunteer', description: 'Help clean stock room and sanitize hospital floors', date: '08/04/2020', location: "New York, NY", time: "9:00AM - 12:00PM", volunteer_id: 2)
