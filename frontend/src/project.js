90a56bd72e6fb5cd83b30242cb66e8f2193926e8
stabassum <sumu1993@gmail.com>
2020-07-23 20:36:06 -0400

    project.js in frontend updated

---
+++ frontend/src/project.js
@@ -0,0 +1,22 @@
+// project objects
+
+class Project {
+    constructor(data){
+        this.title = data.title
+        this.description = data.description
+        this.date = data.date
+        this.location = data.location
+        this.time = data.time
+        this.volunteer_id = data.volunteer_id
+    }
+}
+
+function addProject(){
+
+}
+
+function renderProjectFormFields(volunteerId){
+
+}
+
+function renderNewVolunteerForm(){}
